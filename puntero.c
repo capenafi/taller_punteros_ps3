4a3df6f63edee618fbece11ead852052f6ebe3eb
capenafi <capenafi@espol.edu.ec>
2017-10-30 15:32:14 -0500

    Carlos Penafiel Edgar Moreira

--- puntero.c
+++ puntero.c
@@ -2,14 +2,30 @@
 
 int main(void)
 {
-        int variable = 10;
+	char cadena[100];
+	int numero=0;
+	int i,j=0;
+	printf("Ingrese la palabra a analizar: ");
+	scanf("%s",cadena);
+	numero=strlen(cadena);
+	for(i=0;i<((numero+1)/2);i++){
+		numero--;
+		printf("%c %c",cadena[i],cadena[numero]);
+		if(cadena[i]!=cadena[numero]){
+			j=1;
+			break;
+		}
+	}
+	if(j==1){
+		printf("\nLa palabra no es un palindromo.");
+	}else{
+		printf("\nLa palabra es un palindromo.");
+	}
+
+        /*int variable = 10;
         int *puntero = &variable;
         *puntero = 10;
         printf("&variable : %p\n", &variable);
-        printf("puntero   : %p\n", puntero);
-        printf("&puntero  : %p\n", &puntero);
-        printf("variable  : %d\n", variable);
-        printf("*puntero  : %d\n", *puntero);
-        printf("Tamanio de puntero  : %d bits\n", sizeof(puntero)*8);
+        printf("puntero   : %p\n", puntero);*/
         return 0;
 }
